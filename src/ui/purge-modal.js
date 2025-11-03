be203fac82c982c366e7df389aabae009097c467
00Chaotic <44113229+00Chaotic@users.noreply.github.com>
2025-11-03 12:50:24 +1100

    Add more purge setup UI components
    This commit adds a duration selection and user exclusion selection
    component to the purge setup modal.
    
    The imports have also been condensed into one line where appropriate,
    and the `setRequired(true)` functions have been removed as it is
    already true by default.

--- src/ui/purge-modal.js
+++ src/ui/purge-modal.js
@@ -1,6 +1,4 @@
-const { TextInputBuilder } = require('@discordjs/builders');
-const { ChannelSelectMenuBuilder } = require('@discordjs/builders');
-const { ModalBuilder } = require('@discordjs/builders');
+const { TextInputBuilder, StringSelectMenuBuilder, ChannelSelectMenuBuilder, ModalBuilder, StringSelectMenuOptionBuilder, UserSelectMenuBuilder } = require('@discordjs/builders');
 const { LabelBuilder, TextInputStyle, ChannelType, TextDisplayBuilder } = require('discord.js');
 
 exports.createPurgeSetupModal = function() {
@@ -13,21 +11,41 @@ exports.createPurgeSetupModal = function() {
 
     const channelSelect = new ChannelSelectMenuBuilder()
         .setCustomId('announcementChannelSelect')
-        .setRequired(true)
         .setPlaceholder('Select the channel to post the announcement in')
         .addChannelTypes(ChannelType.GuildText);
 
     const announcementTextInput = new TextInputBuilder()
         .setCustomId('announcementTextInput')
-        .setRequired(true)
         .setStyle(TextInputStyle.Paragraph)
         .setPlaceholder('Announcement message to be displayed. Group pings like @everyone can be used if bot has permissions');
 
+    const durationSelect = new StringSelectMenuBuilder()
+        .setCustomId('durationSelect')
+        .setPlaceholder('Choose the amount of time before the purge occurs')
+        .addOptions(
+            new StringSelectMenuOptionBuilder()
+                .setLabel('1 hour')
+                .setValue('1h'),
+            new StringSelectMenuOptionBuilder()
+                .setLabel('1 day')
+                .setValue('2d'),
+            new StringSelectMenuOptionBuilder()
+                .setLabel('1 week')
+                .setValue('1w'),
+        );
+
+    const excludedUsersSelect = new UserSelectMenuBuilder()
+        .setCustomId('excludedUsersSelect')
+        .setRequired(false)
+        .setPlaceholder('Select any members that should not be purged regardless of activity');
+
     const announcementChannelLabel = new LabelBuilder().setLabel('Announcement Channel').setChannelSelectMenuComponent(channelSelect);
     const announcementTextLabel = new LabelBuilder().setLabel('Announcement Message').setTextInputComponent(announcementTextInput);
+    const durationSelectLabel = new LabelBuilder().setLabel('Duration').setStringSelectMenuComponent(durationSelect);
+    const excludedUsersLabel = new LabelBuilder().setLabel('Excluded Users').setUserSelectMenuComponent(excludedUsersSelect);
 
     modal.addTextDisplayComponents(purgeDescription);
-    modal.addLabelComponents(announcementChannelLabel, announcementTextLabel);
+    modal.addLabelComponents(announcementChannelLabel, announcementTextLabel, durationSelectLabel, excludedUsersLabel);
 
     return modal;
 };
